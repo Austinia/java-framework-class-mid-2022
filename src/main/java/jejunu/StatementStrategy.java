38783a38f6d30da5c5376c1b357e05ad47ce0604
Austinia <kimdy90919@gmail.com>
2022-04-22 18:47:14 +0900

    9번

---
+++ src/main/java/jejunu/StatementStrategy.java
@@ -0,0 +1,9 @@
+package jejunu;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public interface StatementStrategy {
+    public PreparedStatement makeStatement(Object obj, Connection connection) throws SQLException;
+}
